b5f2bc59ee8002f66add86d82fc97a369be6cc6e
Arvin <akhadisurya@gmail.com>
2016-08-01 11:17:51 -0700

    change root to listing picts

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :pictures
   resources :pictures
   devise_for :users
+  root 'pictures#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
